0903e92febe82374926e783e6a4e98b6e028d711
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2024-10-25 21:34:59 +0200

    Add static noise to TV effect

--- scenes/ui/tv_layer.gd
+++ scenes/ui/tv_layer.gd
@@ -1,7 +1,25 @@
 extends CanvasLayer
 
 @onready var _noise := $Noise
+@onready var _static_noise := $StaticNoise
+
+#var just_pressed := false
+#var pressed := false
+#func _physics_process(_delta: float) -> void:
+	#just_pressed = false
+	#if Input.is_action_just_pressed("ui_accept"):
+		#just_pressed = true
+		#pressed = true
+	#if Input.is_action_just_released("ui_accept"):
+		#pressed = false
+	#
+	#if just_pressed:
+		#turn_noise(true)
+		#print("dupa")
+	#if not pressed:
+		#turn_noise(false)
 
 
 func turn_noise(enabled: bool) -> void:
+	_static_noise.playing = enabled
 	_noise.visible = enabled
