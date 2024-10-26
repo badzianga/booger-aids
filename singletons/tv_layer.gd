cb04c74dcfffd08596d2d8426c7b8fbec15d1e6a
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2024-10-26 13:00:17 +0200

    Extra noise for TV layer

--- singletons/tv_layer.gd
+++ singletons/tv_layer.gd
@@ -3,14 +3,18 @@ extends CanvasLayer
 @onready var _white := $White
 @onready var _static_noise := $StaticNoise
 @onready var _timer := $Timer
+@onready var funny_noise := $FunnyNoise
 
 signal noise_finished
 
 
 # TODO: temporary for testing purposes
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
 		enable_noise(randf_range(0.1, 0.4))
+	funny_noise.position.y += delta * 400
+	if funny_noise.position.y >= 1000:
+		funny_noise.position.y = -250
 
 
 func enable_noise(time: float) -> void:
