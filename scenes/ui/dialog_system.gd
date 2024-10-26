671ac2ac7f9d738cf5039d223b8483f47c24a272
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2024-10-26 20:58:13 +0200

    Added descriptions for items

--- scenes/ui/dialog_system.gd
+++ scenes/ui/dialog_system.gd
@@ -1,18 +1,12 @@
 class_name DialogSystem
 extends Control
 
-@export_file("*.json") var dialog_path: String
-
-var _dialog: Dictionary
-
 @onready var _initial_visible := false
 @onready var _text_label := $Box/Margin/TextLabel
 
 
 func _ready() -> void:
 	visible = _initial_visible
-	assert(not dialog_path.is_empty(), "Dialog is not loaded!")
-	_dialog = _load_dialog()
 
 
 func _input(event: InputEvent) -> void:
@@ -25,12 +19,4 @@ func _input(event: InputEvent) -> void:
 
 func display_dialog(id: StringName) -> void:
 	visible = true
-	_text_label.bbcode_text = &"[center]" + _dialog[id] + &"[/center]"
-
-
-func _load_dialog() -> Dictionary:
-	var _file := FileAccess.open(dialog_path, FileAccess.READ)
-	var _content := _file.get_as_text()
-	var _json: Dictionary = JSON.parse_string(_content)
-	assert(typeof(_json) == TYPE_DICTIONARY, "Structure from json is not dictionary!")
-	return _json
+	_text_label.bbcode_text = &"[center]" + GlobalVariables.dialogs[id] + &"[/center]"
--- singletons/global_variables.gd
+++ singletons/global_variables.gd
@@ -1,5 +1,56 @@
 extends Node
 
+const dialogs := {
+	
+}
+
+const descriptions := {
+	"casette": {
+		"name": "\"Let's play a game\" casette",
+		"text": "This weird game is in... game?",
+	},
+	"casette_fuck": {
+		"name": "Let's play a game",
+		"text": "Are you enjoying your game? Make sure to subsKILLbe and not leave any evidence.",
+	},
+	"wig": {
+		"name": "Granny's wig",
+		"text": "A soft, silvery-gray wig styled in gentle curls, reminiscent of Granny's classic look and comforting presence.",
+	},
+	"wig_fuck": {
+		"name": "Old Hag's wig",
+		"text": "A frizzy, dusty-gray wig with thinning curls, looking like it’s seen a few too many decades on an old hag's head.",
+	},
+	"leg": {
+		"name": "Grandpa's peg leg",
+		"text": "A well-worn prosthetic, sturdy and crafted with care, representing Grandpa's resilience and journey through the years.",
+	},
+	"leg_fuck": {
+		"name": "Geezer's leg",
+		"text": "A symbol of the old man's strange preferences.",
+	},
+	"puppets": {
+		"name": "You",
+		"text": "You danced to their tune. It's good that in the end you danced on their grave.",
+	},
+	"doll": {
+		"name": "Broken doll",
+		"text": "A cherished, timeworn doll with a sweet, faded smile. Unfortunately, her original, beautiful eyes are missing.",
+	},
+	"doll_fuck": {
+		"name": "Not-Anymore-Eyeless doll",
+		"text": "This old man destroyed your precious treasure. So you destroyed him to ger your treasure a new eyes.",
+	},
+	"finger": {
+		"name": "Severed finger",
+		"text": "A small, preserved finger with softened lines and a curious stillness, holding the quiet memory of a once-busy hand.",
+	},
+	"finger_fuck": {
+		"name": "Her finger",
+		"text": "Her finger.",
+	},
+}
+
 var current_interactable: Interactable = null
 var fuck_up_level := 0  # a.k.a. collectibles found
 var player: Player = null
