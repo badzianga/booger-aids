b36541bbef873a876ab625c1139cabbff527b054
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2024-10-26 11:00:42 +0200

    Remove unused code

--- singletons/tv_layer.gd
+++ singletons/tv_layer.gd
@@ -4,22 +4,6 @@ extends CanvasLayer
 @onready var _static_noise := $StaticNoise
 @onready var _timer := $Timer
 
-#var just_pressed := false
-#var pressed := false
-#func _physics_process(_delta: float) -> void:
-	#just_pressed = false
-	#if Input.is_action_just_pressed("ui_accept"):
-		#just_pressed = true
-		#pressed = true
-	#if Input.is_action_just_released("ui_accept"):
-		#pressed = false
-	#
-	#if just_pressed:
-		#turn_noise(true)
-		#print("dupa")
-	#if not pressed:
-		#turn_noise(false)
-
 
 # TODO: temporary for testing purposes
 func _physics_process(_delta: float) -> void:
